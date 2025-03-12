c297f7cd9354bb6369a8acf2dc04e8c5375cb1a0
malikakika <malikachoubri@gmail.com>
2025-03-12 15:04:16 +0100

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,12 @@
-// @ts-check
+const { withNx } = require('@nx/next');
+const withPWA = require('next-pwa');
 
-const { composePlugins, withNx } = require('@nx/next');
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Active la PWA en production uniquement
-});
-
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-  nx: {
-    svgr: false,
+const nextConfig = withNx(withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
   },
-};
+}));
 
-const plugins = [withNx, withPWA];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = nextConfig;
